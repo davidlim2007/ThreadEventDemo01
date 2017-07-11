1f760ccab5d1abd7370fab0dad7181aac68816b2
davidlim2007 <lim.david2007@gmail.com>
2017-07-11 23:51:08 +0800

    Code restructuring and additional comments
    Added extra comments to further clarify how the code works.
    
    Shifted Main() method to the bottom (for consistency with other
    projects)

--- Program.cs
+++ Program.cs
@@ -7,32 +7,9 @@ using System.Threading;
 
 namespace ThreadEventDemo01
 {
-    // TODO: Add comments.
-    //
-    // Program flow:
-    //
-    // 1. Start two threads.
-    //
-    // 2. Thread 1 monitors the file system to look out for a file. It operates in a loop.
-    //
-    // 2.1 When the file is found, m_event_file_arrived is set. Thread 1 breaks out of loop.
-    //
-    // 2.2 Else, Thread 1 blocks for 1 second and the loop reiterates.
-    //
-    // 3. Thread 2 waits on the event to be set. Once event is set, it reads from the file
-    // and displays its contents on the console.
-
     class Program
     {
         static string filepath = @"D:\David\test\my_file.txt";
-
-        static void Main(string[] args)
-        {
-            StartThread01();
-            StartThread02();
-            WaitThread01ToEnd();
-            WaitThread02ToEnd();
-        }
 
         static void StartThread01()
         {
@@ -49,14 +26,15 @@ namespace ThreadEventDemo01
         // This method represents the entry-point for Thread 01.
         //
         // Thread 01 monitors the file system for the existence of a
-        // file. 
+        // file in a directory. 
         //
-        // In a while-loop, it checks for the existence of the
-        // file. If the file is not found, the thread goes to
-        // sleep for 1 second before the loop reiterates.
+        // In a while-loop, it checks for the existence of a
+        // specific file. If the file is not found, the thread goes to
+        // sleep for 1 second and then the loop reiterates.
         //
         // If the file is found, the thread sets the m_event_file_arrived
-        // event to signalled state and breaks out of the loop.
+        // event to the signalled state and breaks out of the loop.
+        // The thread ends naturally thereafter.
         static void WorkerThreadMethod01()
         {
             Console.WriteLine("Waiting for file...");
@@ -69,6 +47,7 @@ namespace ThreadEventDemo01
                 }
                 else
                 {
+                    // File found. Set the event.
                     m_event_file_arrived.Set();
                     break;
                 }
@@ -77,19 +56,19 @@ namespace ThreadEventDemo01
 
         // This method represents the entry-point for Thread 02.
         //
-        // Thread 02 reads from the specified file upon its discovery.
+        // Thread 02 reads from a specified file upon its discovery.
         //
         // Firstly, it waits for the m_event_file_arrived event to be
-        // signalled (i.e. when the file is found on the system).
+        // signalled (i.e. when the file is found in a directory).
         //
         // Once the event is signalled, the thread reads from the file
         // and displays its contents on the console.
         static void WorkerThreadMethod02()
         {
-            // Much like Mutexes, the ManualResetEvent class
+            // Just like Mutexes, the ManualResetEvent class
             // has a WaitOne() method. This method blocks the
             // calling thread until the event is set to
-            // signalled state.
+            // the signalled state.
             m_event_file_arrived.WaitOne();
 
             // Note: While not absolutely necessary, it is a recommended 
@@ -108,6 +87,14 @@ namespace ThreadEventDemo01
         static void WaitThread02ToEnd()
         {
             m_thread_02.Join();
+        }
+
+        static void Main(string[] args)
+        {
+            StartThread01();
+            StartThread02();
+            WaitThread01ToEnd();
+            WaitThread02ToEnd();
         }
 
         private static Thread m_thread_01 = null;
